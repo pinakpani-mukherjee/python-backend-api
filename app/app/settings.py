1f3337726fe12e3340f8503d4510936fb7bc231b
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-09-08 00:20:34 +0900

    moved secret_key

--- app/app/settings.py
+++ app/app/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = True
 
 ALLOWED_HOSTS = []
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 
 # Application definition
