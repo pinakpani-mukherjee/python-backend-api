6d1a96534e0e001a325ffaa0d4edc0c048f94a23
mukherjee-pinakpani <mukherjee.pinakpani14@gmail.com>
2023-09-17 14:32:22 +0900

    added me tests

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,9 +1,10 @@
 """Serializes for the Users API View
 """
 
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializers(serializers.ModelSerializer):
@@ -18,3 +19,26 @@ class UserSerializers(serializers.ModelSerializer):
         """Create and return a user with encrypted passwords"""
 
         return get_user_model().objects.create_user(**validated_data)
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the iser auth token."""
+
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style={"input_type": "password"}, trim_whitespace=False
+    )
+
+    def validate(self, attrs):
+        email = attrs.get("email")
+        password = attrs.get("password")
+        user = authenticate(
+            request=self.context.get("request"), username=email, password=password
+        )
+
+        if not user:
+            msg = _("Unable to authenticate with provided credentials")
+            raise serializers.ValidationError(msg, code="authorization")
+
+        attrs["user"] = user
+        return attrs
--- app/user/views.py
+++ app/user/views.py
@@ -1,8 +1,15 @@
 """Views for the user API
 """
 from rest_framework import generics
-from user.serializers import UserSerializers
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializers, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializers
+
+
+class CreateTokenView(ObtainAuthToken):
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
